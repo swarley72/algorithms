35a3d35e484d83616eaad2bd3e2ecfb2a98f0652
Dmitrii Kim <kim.dima.93@gmail.com>
2025-01-26 13:17:16 +0300

    1047

---
+++ stack/1047_remove_all_adjacent_duplicates.py
@@ -0,0 +1,9 @@
+def remove_duplicates(s: str) -> str:
+    stack = []
+    for c in s:
+        if not stack or stack[-1] != c:
+            stack.append(c)
+        else:
+            stack.pop()
+
+    return "".join(stack)
