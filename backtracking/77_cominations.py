f9ce20940ca28e2d4bb09524ae86f07b8558b658
swarley72 <kim.dima.93@gmail.com>
2025-01-26 11:45:34 +0300

    77

---
+++ backtracking/77_cominations.py
@@ -0,0 +1,16 @@
+def combine(n: int, k: int) -> list[list[int]]:
+    result = []
+
+    def backtrack(acc, start):
+        if len(acc) == k:
+            result.append(acc[:])
+            return
+
+        for num in range(start, n + 1):
+            if num not in acc:
+                acc.append(num)
+                backtrack(acc, num + 1)
+                acc.pop()
+
+    backtrack([], 1)
+    return result
