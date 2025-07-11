5880b49a17c9bcc5ef9caca5928ec8ae1b0c4647
swarley72 <kim.dima.93@gmail.com>
2025-07-12 01:16:23 +0300

    206-go

---
+++ linked_list/206_reverse_linked_list.go
@@ -0,0 +1,18 @@
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	// если сделать тут просто prev := &ListNode{}
+	var prev *ListNode = nil
+	current := head
+	for current != nil {
+		tmpNext := current.Next
+		current.Next = prev
+		prev = current
+		current = tmpNext
+	}
+
+	return prev
+}
