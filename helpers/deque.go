b24bfe97a2f5cb9af5d14f4d26ae391a7ea177a6
swarley72 <kim.dima.93@gmail.com>
2025-07-13 21:57:50 +0300

    deque linked list

--- helpers/deque.go
+++ helpers/deque.go
@@ -1,23 +1,64 @@
+
+func makeDeque[T any](items []T) *Deque[T] {
+	head := &Node[T]{}
+	tail := &Node[T]{}
+
+	current := head
+	for _, item := range items {
+		node := &Node[T]{Value: item}
+		current.Next = node
+		node.Prev = current
+		current = node
+	}
+	current.Next = tail
+	tail.Prev = current
+	return &Deque[T]{Head: head, Tail: tail, Size: len(items)}
+}
+
+type Node[T any] struct {
+	Value T
+	Next  *Node[T]
+	Prev  *Node[T]
+}
+
 type Deque[T any] struct {
-	items []T
+	Head *Node[T]
+	Tail *Node[T]
+	Size int
 }
 
 func (d *Deque[T]) Append(item T) {
-	d.items = append(d.items, item)
+	node := &Node[T]{Value: item}
+	node.Next = d.Tail
+	node.Prev = d.Tail.Prev
+	d.Tail.Prev.Next = node
+	d.Tail.Prev = node
+	d.Size++
 }
 
 func (d *Deque[T]) AppendLeft(item T) {
-	d.items = append([]T{item}, d.items...)
+	node := &Node[T]{Value: item}
+	node.Next = d.Head.Next
+	d.Head.Next.Prev = node
+	d.Head.Next = node
+	d.Size++
 }
 
 func (d *Deque[T]) Pop() T {
-	pop := d.items[len(d.items)-1]
-	d.items = d.items[:len(d.items)-1]
-	return pop
+	popValue := d.Tail.Prev.Value
+	node := d.Tail.Prev
+	node.Prev.Next = d.Tail
+	d.Tail.Prev = node.Prev
+	d.Size--
+	return popValue
 }
 
 func (d *Deque[T]) PopLeft() T {
-	pop := d.items[0]
-	d.items = d.items[1:]
-	return pop
+	popValue := d.Head.Next.Value
+
+	node := d.Head.Next
+	node.Next.Prev = d.Head
+	d.Head.Next = node.Next
+	d.Size--
+	return popValue
 }
