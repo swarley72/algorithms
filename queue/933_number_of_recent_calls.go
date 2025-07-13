ffb73408dcd55a21ab0b60ff516925aabf687586
swarley72 <kim.dima.93@gmail.com>
2025-07-13 22:08:32 +0300

    933-go

---
+++ queue/933_number_of_recent_calls.go
@@ -0,0 +1,89 @@
+
+func makeDeque[T any](items []T) *Deque[T] {
+	head := &Node[T]{}
+	tail := &Node[T]{}
+
+	current := head
+	for _, item := range items {
+		node := &Node[T]{Value: item}
+		current.Next = node
+		node.Prev = current
+		current = node
+	}
+	current.Next = tail
+	tail.Prev = current
+	return &Deque[T]{Head: head, Tail: tail, Size: len(items)}
+}
+
+type Node[T any] struct {
+	Value T
+	Next  *Node[T]
+	Prev  *Node[T]
+}
+
+type Deque[T any] struct {
+	Head *Node[T]
+	Tail *Node[T]
+	Size int
+}
+
+func (d *Deque[T]) Append(item T) {
+	node := &Node[T]{Value: item}
+	node.Next = d.Tail
+	node.Prev = d.Tail.Prev
+	d.Tail.Prev.Next = node
+	d.Tail.Prev = node
+	d.Size++
+}
+
+func (d *Deque[T]) AppendLeft(item T) {
+	node := &Node[T]{Value: item}
+	node.Next = d.Head.Next
+	d.Head.Next.Prev = node
+	d.Head.Next = node
+	d.Size++
+}
+
+func (d *Deque[T]) Pop() T {
+	popValue := d.Tail.Prev.Value
+	node := d.Tail.Prev
+	node.Prev.Next = d.Tail
+	d.Tail.Prev = node.Prev
+	d.Size--
+	return popValue
+}
+
+func (d *Deque[T]) PopLeft() T {
+	popValue := d.Head.Next.Value
+
+	node := d.Head.Next
+	node.Next.Prev = d.Head
+	d.Head.Next = node.Next
+	d.Size--
+	return popValue
+}
+
+func (d *Deque[T]) GetLeft() T {
+	return d.Head.Next.Value
+}
+
+func (d *Deque[T]) GetRight() T {
+	return d.Tail.Prev.Value
+}
+
+type RecentCounter struct {
+	queue *Deque[int]
+}
+
+func Constructor() RecentCounter {
+	return RecentCounter{makeDeque([]int{})}
+}
+
+func (this *RecentCounter) Ping(t int) int {
+	this.queue.Append(t)
+	windowStart := t - 3000
+	for this.queue.Size > 0 && this.queue.GetLeft() < windowStart {
+		this.queue.PopLeft()
+	}
+	return this.queue.Size
+}
