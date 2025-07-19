c1e9d02f5471ecb25c32373bd26f59d0380cfc03
swarley72 <kim.dima.93@gmail.com>
2025-07-19 12:00:15 +0300

    1971-go

---
+++ graphs/1971_find_if_path_exists_in_graph.go
@@ -0,0 +1,78 @@
+func validPath(n int, edges [][]int, source int, destination int) bool {
+	graph := map[int][]int{}
+
+	for _, edge := range edges {
+		a := edge[0]
+		b := edge[1]
+		graph[a] = append(graph[a], b)
+		graph[b] = append(graph[b], a)
+	}
+
+	seen := NewSet[int]()
+	seen.Add(source)
+	stack := NewStack[int]()
+	stack.Append(source)
+
+	for len(stack) > 0 {
+		v := stack.Pop()
+		if v == destination {
+			return true
+		}
+
+		for _, ngh := range graph[v] {
+			if !seen.Has(ngh) {
+				stack.Append(ngh)
+				seen.Add(ngh)
+			}
+		}
+	}
+
+	return false
+}
+
+// TYPES
+type Set[T comparable] struct {
+	elements map[T]struct{}
+}
+
+func (s *Set[T]) Add(element T) {
+	s.elements[element] = struct{}{}
+}
+
+func (s *Set[T]) Has(element T) bool {
+	_, ok := s.elements[element]
+	return ok
+}
+
+func (s *Set[T]) Remove(element T) {
+	delete(s.elements, element)
+}
+
+func NewSet[T comparable]() *Set[T] {
+	return &Set[T]{elements: make(map[T]struct{})}
+}
+
+type Stack[T any] []T
+
+// удаляет и возвращает последний элемент стека
+func (s *Stack[T]) Pop() T {
+	pop := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return pop
+}
+
+// возвращает последний элемент стека
+func (s Stack[T]) Top() T {
+	return s[len(s)-1]
+}
+
+// добавляет элемент в стэк
+func (s *Stack[T]) Append(val T) {
+	*s = append(*s, val)
+}
+
+func NewStack[T any]() Stack[T] {
+	return Stack[T]{}
+}
+
+
